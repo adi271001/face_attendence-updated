7e0bf5c1cd7447efda26e07044f7d4bee8a2ddc4
adi271001 <67856422+adi271001@users.noreply.github.com>
2020-08-28 19:21:34 +0530

    Update models.py

--- attendence_system/models.py
+++ attendence_system/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.db import models
-from main.models import Employee, EmployeeWork
+from django.contrib.auth.models import User
 
-
-class EmployeePresence(models.Model):
-    Employee = models.OneToOneField(Student, models.CASCADE)
-    present = models.BooleanField(default=True)
-    Employee_work = models.ForeignKey(EmployeeWork, on_delete=models.CASCADE)
-    date = models.DateField(auto_now=True)
-    # appear in the least again
+import datetime
 # Create your models here.
+
+
+class Present(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	date = models.DateField(default=datetime.date.today)
+	present=models.BooleanField(default=False)
+	
+class Time(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	date = models.DateField(default=datetime.date.today)
+	time=models.DateTimeField(null=True,blank=True)
+	out=models.BooleanField(default=False)
