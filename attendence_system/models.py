0a5b65065abe82ea06b372b04f6092a471ca722a
adi271001 <67856422+adi271001@users.noreply.github.com>
2020-08-28 19:22:03 +0530

    Update models.py

--- attendence_system/models.py
+++ attendence_system/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 import datetime
 # Create your models here.
-
-
 class Present(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	date = models.DateField(default=datetime.date.today)
-	present=models.BooleanField(default=False)
-	
+	present=models.BooleanField(default=False)	
 class Time(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	date = models.DateField(default=datetime.date.today)
